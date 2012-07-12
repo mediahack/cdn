f43808fc9510779f825541528f5c28881a22dfd3
mediahack <code@mediahack.com>
2012-07-12 15:27:50 -0500

    removing stupid stuff. updating paths to js files.

--- projects/mediahack/tumblr/themes/gameboy/js/gb.js
+++ projects/mediahack/tumblr/themes/gameboy/js/gb.js
@@ -2,5 +2,3 @@ var currentNav = document.location.pathname.replace(/\/$/,'');
 if( document.location.pathname != '/'){
 	jQuery('nav.main a[href^=' + currentNav + ']').addClass('current');
 }
-
-[feedly mini] 
